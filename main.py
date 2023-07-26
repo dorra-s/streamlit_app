e6d1852c944f7c03dbf073565f5efeb505e3a0f4
dorra <dorrasadalah19@gmail.com>
2023-07-26 16:25:21 +0100

    updates

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import dask.dataframe as dd
 #/home/dorra/.local/bin/streamlit run main.py
 
 
-# In[4]:
+# In[3]:
 
 
 df_InfoUser  = dd.read_csv(r'/media/dorra/62207C0F207BE885/Users/ASUS/Desktop/archive/Info_UserData.csv')
@@ -59,12 +59,10 @@ total_rows_InfoUser = len(df_InfoUser)
 total_rows_LogProblem = len(df_LogProblem)
 total_rows_InfoContent = len(df_InfoContent)
 
-# Calculate the percentage of missing values for each column
 percentage_missing_InfoUser = (missing_values_InfoUser / total_rows_InfoUser) * 100
 percentage_missing_LogProblem = (missing_values_LogProblem / total_rows_LogProblem) * 100
 percentage_missing_InfoContent = (missing_values_InfoContent / total_rows_InfoContent) * 100
 
-# Filter columns with missing values percentage greater than 1%
 filtered_missing_InfoUser = percentage_missing_InfoUser[percentage_missing_InfoUser > 1]
 filtered_missing_LogProblem = percentage_missing_LogProblem[percentage_missing_LogProblem > 1]
 filtered_missing_InfoContent = percentage_missing_InfoContent[percentage_missing_InfoContent > 1]
@@ -112,28 +110,15 @@ num_not_self_coach = len(not_self_coach)
 print("has 0 students, 0 teachers and not self coach:",num_not_self_coach)
 
 
-# In[7]:
+# In[6]:
 
 
-outlier_columns = ['total_sec_taken', 'total_attempt_cnt', 'used_hint_cnt']
+#LogProblem
+df_zero_sec_taken = df_LogProblem[df_LogProblem['total_sec_taken'] == 0]
 
-grouped_log_problem = df_LogProblem.groupby('uuid')
+num_users_with_zero_sec_taken = df_zero_sec_taken['uuid'].nunique().compute()
 
-unique_uuids = df_InfoUser['uuid'].unique().compute()
-
-plt.figure(figsize=(12, 6))
-
-for i, col in enumerate(outlier_columns, 1):
-    plt.subplot(1, 3, i)
-    for uuid in unique_uuids:
-        group = grouped_log_problem.get_group(uuid)
-        sns.stripplot(x=[uuid] * len(group), y=col, data=group, jitter=True, color='green', alpha=0.7)
-    plt.xlabel('uuid')
-    plt.ylabel(col)
-    plt.title(f'Strip Plot - {col}')
-    plt.tight_layout()
-
-plt.show()
+print("Number of users with 0 seconds taken:", num_users_with_zero_sec_taken)
 
 
 # INSIGHTS:
@@ -142,43 +127,12 @@ plt.show()
 # 
 #    .Users who accessed the problem but did not engage in any interaction, possibly indicating a lack of interest or motivation.
 #    .Users who navigated to the problem but left the platform or session before attempting it.
-# 2_Users with 0 'total_attempt_cnt': Users with a total_attempt_cnt of 0 likely indicate cases where the users did not make any attempts to solve the problem. Insights from these cases could include:
-# 
-#    .Users who accessed the problem but did not actively interact or attempt to solve it.
-#    .Users who might have skipped or ignored the problem intentionally.
 # 
 # Possible actions or improvements could include:
 # 
 #   Identifying potential usability issues or design flaws in the platform that might discourage users from attempting or interacting with problems.
 # 
 #   Providing additional guidance or incentives to encourage users to actively attempt more problems and engage in learning activities.
-
-# In[8]:
-
-
-zero_sec_taken = df_LogProblem[df_LogProblem['total_sec_taken'] == 0]
-zero_attempt_cnt = df_LogProblem[df_LogProblem['total_attempt_cnt'] == 0]
-
-zero_sec_taken_hint_count = zero_sec_taken['used_hint_cnt'].value_counts()
-zero_attempt_cnt_hint_count = zero_attempt_cnt['used_hint_cnt'].value_counts()
-
-plt.figure(figsize=(10, 6))
-
-plt.subplot(1, 2, 1)
-plt.bar(zero_sec_taken_hint_count.index, zero_sec_taken_hint_count.values)
-plt.xlabel('Number of Used Hints')
-plt.ylabel('Number of Users')
-plt.title('Number of Users with 0 Seconds Taken')
-
-plt.subplot(1, 2, 2)
-plt.bar(zero_attempt_cnt_hint_count.index, zero_attempt_cnt_hint_count.values)
-plt.xlabel('Number of Used Hints')
-plt.ylabel('Number of Users')
-plt.title('Number of Users with 0 Attempt Count')
-
-plt.tight_layout()
-plt.show()
-
 
 # # The distribution of students across different learning stages:
 
